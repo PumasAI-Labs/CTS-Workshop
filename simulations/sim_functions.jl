90d1806abe7a24f2b94a6409e1b7a3ab27e088ea
htbunn <haden@pumas.ai>
2023-10-11 20:25:28 +0000

    complete function test and update sim functions

--- simulations/sim_functions.jl
+++ simulations/sim_functions.jl
@@ -91,7 +91,7 @@ function create_patient_dict(dfr)
         "dose_tapered_in_cycle" => false,
         "interrupt" => false, # interrupt always leads to at least 1 DT
         "pdc_cnt" => 0,
-        "stat_dc" => false,
+        "statdc" => false,
         "at_goal_prev_cycle" => false,
         "covariates" => create_covariate_dict(dfr),
         "models" => dfr.models,
@@ -291,38 +291,61 @@ function evaluate_patient(pt)
     # named tuple with current day's eval metrics (sdma, plt)
     pd = pt["current_pd"]
 
+    #= 
+        * This is an escape condition; a way to bypass all of the logic that follows because
+        * every if-else block below this one has the ability to modify therapy in some way.
+        * NO_DOSE_ADJ gives us access to the "regular" sim that was performed in the mansucript
+        * CDAY = 7 & SKIP_CDAY7 let us skip the "visit" at CDAY 7 (sometimes it's easier to code for when NOT to do something)
+        * - need to specify CDAY along with the skip or anytime SKIP_CDAY7 is true, the script will simply continue without evaluating the therapy
+        * Always skip the "visit" at CDAY 21
+    =# 
+    # If any of these are TRUE, continue current therapy without any modification or status change
     if pt["scenario"].NO_DOSE_ADJ || (pt["cycle_day"] == 7 && pt["scenario"].SKIP_CDAY7) || pt["cycle_day"] == 21
+        #//@info "continue_therapy 1"
         return continue_therapy(pt)
     end
 
     # check safety (any PLT value <G3 threshold considered "unsafe")
     if pd.plt < pt["scenario"].PLT_G3
         if pd.plt < pt["scenario"].PLT_G4
+            #//@info "discontinue_therapy"
             return discontinue_therapy(pt)
         else
+            #//@info "interrupt therapy"
             return interrupt_therapy(pt)
         end
     end
 
-    # check for interrupt and restart (restart if PLT ≥ G3)
+    # check for interrupt and restart if PLT ≥ G3 threshold
     if pt["interrupt"]
+        #//@info "restart_therapy"
         return restart_therapy(pt, -1)
     end
-
+    #=
+        * Option to immediately taper the dose of a patient with G2 CIT, but not G3 in effort to prevent AE from progressing
+        * Only available at CDAY 7 or 15
+        * Not allowed if they're already on the lowest dose (2mg) or if they've already been tapered at another point during the current cycle
+    =#
     # check whether patient is at risk for SAE (G2 CIT)
-    if pt["cycle_day"] ≠ 28 && (pd.plt < pt["scenario"].PLT_G2 && pt["scenario"].ADJUST_G2) && (!pt["dose_tapered_in_cycle"] && pt["current_dose"] > 2)
+    if pt["cycle_day"] ≠ 28 && (pd.plt < pt["scenario"].PLT_G2 && pt["scenario"].G2_ADJUST) && (!pt["dose_tapered_in_cycle"] && pt["current_dose"] > 2)
+        #//@info "modify_therapy"
         return modify_therapy(pt, -1)
     end
 
-    #
-    if pt["cycle_day"] == 28 # rule is less sensitive and will eval UT every 12w regardless of duration at current dose
+    #=
+        * Titration to higher dose only available on CDAY28
+        * CDAY should not be able to exceed 28, but as a failsafe, it will evaluate as continue_therapy and cycle will increase at the end of the loop resetting CDAY
+
+    =#
+    if pt["cycle_day"] == 28
+        #//@info "evaluate_therapy"
         return evaluate_efficacy(pt)
     else
+        #//@info "continue_therapy 2"
         return continue_therapy(pt)
     end
 
 end
-
 
 
 function sim_trial(dfr, ncycles)
@@ -445,7 +468,150 @@ function sim_trial(dfr, ncycles)
             cycle += 1
             pt["cycle_day"] = 0
             # once this evaluates to true, it doesn't repeat the evaluation again
-            pt["at_goal_prev_cycle"] = !pt["at_goal_prev_cycle"] && (1-(trial_event.sdma/pt["sdma0"]) < pt["scenario"].EFF_TARGET) ? true : false
+            pt["at_goal_prev_cycle"] = !pt["at_goal_prev_cycle"] && (1-(trial_event.sdma/pt["sdma0"]) ≥ pt["scenario"].EFF_TARGET/100) ? true : false
+        end
+
+    end
+
+    # store final trial events vector
+    pt["trial_events"] = trial_events
+    pt["regimen"] = regimen
+
+    # return patient object
+    return pt
+
+end
+
+
+
+function sim_trial_validation(pt, ncycles)
+    #* INPUTS MODIFIED FOR VALIDATION PURPOSES
+    #* DO NOT USE FOR REGULAR SIMULATION
+    # set cycle counter
+    cycle = 1
+
+    #! Modified for validation
+    # create patient dictionary
+    #//pt = create_patient_dict(dfr)
+
+    # set starting regimen
+    regimen = DosageRegimen(
+        pt["scenario"].INIT_REG.AMT * 1000; #mg to μg
+        evid = 1,   
+        time = pt["scenario"].INIT_REG.TIME,
+        cmt = 1,
+        ii = 24,
+        addl = pt["scenario"].INIT_REG.ADDL
+    )
+
+    # create first trial event; i.e. begin treatment
+    trial_event = initiate_therapy(pt)
+
+    # add trial day and duration of current dose to trial_event and store in vector for later
+    # ! remember dict elements are passed by ref and pt["cycle_day"] may cause issues here
+    trial_events = [(trial_event..., cycle = cycle, day = pt["trial_day"], cycle_day = pt["cycle_day"], current_dose_dur = 0)]
+
+    # loop through and evaluated patients q4w beginning at 4w through ndays
+    while cycle ≤ ncycles
+        
+        # check patient status to confirm eligible to continue
+        if pt["status"] != 1
+            break
+        end
+
+        # set first non-zero dose given at start of current cycle which will be reference for flagging taper
+        pt["dose_at_cycle_start"] = pt["cycle_day"] == 0 ? pt["lnzdose"][end] : pt["dose_at_cycle_start"]
+
+        #trial_day += 1 # * this version is for incrementing by 1 trial day
+        pt["trial_day"] = pt["trial_day"] + pt["scenario"].RECHECK_SCHED
+
+        # * here, cycle day is defined as a day where the patient received drug (i.e., current_dose > 0)
+        # update cycle day
+        pt["cycle_day"] = pt["current_dose"] > 0 ? pt["cycle_day"] + pt["scenario"].RECHECK_SCHED : pt["cycle_day"]
+
+        # simulate PK/PD through current day
+        # * this is the 'dose administration' step and conceptually, is what 'moves' the trial day forward
+        #! Disabled this input for validation, will pass pt["profile"] as an external vector
+        #//pt["profile"] = sim_profile(pt, regimen)
+
+        #! Disabled for validation
+        #=
+        # add SDMA0 and PLT0 to patient after first sim_profile run
+        if pt["trial_day"] == 7
+            pt["sdma0"] = pt["profile"].icoefs.sdma0[end]
+            pt["plt0"] = pt["profile"].icoefs.circ0[end]
+        end
+        =#
+
+        # update duration at current dose
+        # TODO: explain why this is done in this order (b/c the dose isn't 'given' until sim_profile is called)
+        pt["current_dose_dur"] = pt["current_dose"] == pt["previous_dose"] ? 
+            pt["current_dose_dur"] += pt["scenario"].RECHECK_SCHED : # ? may be better to fix this to be +x; where x is a standard time interval (ie, 7d)
+            pt["scenario"].RECHECK_SCHED # ? may be better to fix this along with the above
+
+        #! Modified for validation
+        # named tuple of current pd values (sdma, plt)
+        #//pt["current_pd"] = get_current_pd(pt["profile"])
+        pt["current_pd"] = filter(ntp -> ntp.day == pt["trial_day"], pt["profile"])[1]
+
+        # evaluate current dose through current day and determine next dose
+        trial_event = evaluate_patient(pt)
+
+        # add trial_event to trial_events
+        push!(trial_events, (trial_event..., cycle = cycle, day = pt["trial_day"], cycle_day = pt["cycle_day"], current_dose_dur = pt["current_dose_dur"]))
+
+        # update interrupt status post eval
+        pt["interrupt"] = trial_event.interrupt
+
+        # update dose information
+        pt["current_dose"] = trial_event.new_dose # new becomes current
+        pt["previous_dose"] = trial_event.cur_dose # current becomes previous
+
+        # flag indicating whether dose was tapered at any point during current cycle
+        pt["dose_tapered_in_cycle"] = !iszero(trial_event.new_dose) && trial_event.new_dose < pt["dose_at_cycle_start"]
+
+        # check if patient flagged for permanent dc d/t G4 AE
+        if trial_event.statdc == true
+            pt["status"] = 2
+            continue
+        end
+
+        # check if therapy was interrupted
+        # increase counter if interrupted at 2mg and flag for d/c if count > 1
+        if trial_event.interrupt == true
+            # if interrupted while on 2mg increase d/c count by 1
+            if pt["previous_dose"] == 2.0
+                pt["pdc_cnt"] += 1
+                # if d/c count reaches 2, d/c therapy
+                if pt["pdc_cnt"] == 2
+                    pt["status"] = 2
+                    continue
+                end
+            end
+        end
+
+        # update dosing regimen to include next dose
+        regimen = update_regimen(regimen, trial_event, pt["trial_day"])
+
+        # first lnzdose value is set to first dose
+        # used to track non-zero doses and when restarting tx
+        pt["lnzdose"] = push!(
+            pt["lnzdose"],
+            pt["current_dose"] > 0 ? pt["current_dose"] : pt["lnzdose"][end]
+        )
+
+        # update lnzdose and ddi_flags if trial_event.new_dose = 0
+        if trial_event.new_dose == 0
+            pt["lnzdose"] = append!(pt["lnzdose"], repeat([pt["lnzdose"][end]], 6))
+        end
+
+        # increase cycle and reset cycle day
+        # ! using ≥28 for now as a defensive measure to make sure while-loop isn't infinite
+        if pt["cycle_day"] ≥ 28
+            cycle += 1
+            pt["cycle_day"] = 0
+            # once this evaluates to true, it doesn't repeat the evaluation again
+            pt["at_goal_prev_cycle"] = !pt["at_goal_prev_cycle"] && (1-(trial_event.sdma/pt["sdma0"]) ≥ pt["scenario"].EFF_TARGET/100) ? true : false
         end
 
     end
--- simulations/sim_functions_test.jl
+++ simulations/sim_functions_test.jl
@@ -1,13 +1,22 @@
 ##################################################################
+# Load Packages
+##################################################################
+using Pumas
+using DataFramesMeta
+
+##################################################################
 # Helper Functions and Data
 ##################################################################
 
-# models
-integrated = include("integrated.jl");
+include("sim_functions.jl")
 
-SCENARIO1 = (
+# FIXME: update this with the final sim combined model, not the ref
+combined_pkpd = include("../setup/combined_pkpd_ref.jl")
+
+SCENARIO = (
     NO_DOSE_ADJ = false, # disable dose adjustment logic and simulate all cycles at 1 dose
     SKIP_CDAY7 = false,
+    G2_ADJUST = false,
     PLT_G4 = 25,
     PLT_G3 = 50,
     PLT_G2 = 75,
@@ -22,8 +31,8 @@ patient_df = DataFrame(
     id = 1,
     tdd = 4,
     freqn = 24,
-    scenario = SCENARIO1,
-    models = (; integrated)
+    scenario = SCENARIO,
+    models = (; combined_pkpd)
 );
 
 
@@ -73,7 +82,7 @@ update_regimen(reg, (; new_dose = 4.0), 7)
 reg = reset_regimen()
 
 # update regimen to include day 7-14 dosing
-updated_reg = update_regimen(reg, (; new_dose, 4.0), 7)
+updated_reg = update_regimen(reg, (; new_dose = 4.0), 7)
 
 # test patient
 testpatient = reset_patient()
@@ -91,7 +100,8 @@ testpatient["covariates"]
 ##################################################################
 # sim_profile & get_current_pd
 ##################################################################
-
+#* This block is working, but slows down the debugger; uncomment to run if change simp_rofile
+#=
 reg = reset_regimen();
 testpt = reset_patient();
 # modified sim_profile so that number of days to sim is determined by pt["trial_day"]
@@ -104,7 +114,7 @@ testsim = sim_profile(testpt, reg)
 # get_current_pd takes SimulatedObservations as its input
 # expect a named tuple with elements for sdma and plt
 pd = get_current_pd(testsim);
-
+=#
 ##################################################################
 # modify_dose
 ##################################################################
@@ -207,26 +217,15 @@ evaluate_efficacy(testpt)
 
 
 # NOT AT GOAL, CYCLE 2 (at goal previously) CONTINUE AT CURRENT DOSE
-SCENARIO2 = (
-    NO_DOSE_ADJ = false, # disable dose adjustment logic and simulate all cycles at 1 dose
-    SKIP_CDAY7 = false,
-    PLT_G4 = 25,
-    PLT_G3 = 50,
-    PLT_G2 = 75,
-    RESP_VARS = (SDMA = :sdma, PLT = :plt), # :sdma
-    EFF_TARGET = 78, # 78% reduction in SDMA from baseline
-    EFF_TARGET_OFFSET = 30, # % reduction in efficacy target (0 = none)
-    RECHECK_SCHED = 7, # days between scheduled evaluations of PLT
-    INIT_REG = (AMT = 4.0, TIME = 0, II = 24, ADDL = 6), # ADDL = 6; weekly observations
-);
+TEMPSCEN = (; SCENARIO..., EFF_TARGET_OFFSET = 30)
 
 testpt = create_patient_dict(
     DataFrame(
         id = 1,
         tdd = 4,
         freqn = 24,
-        scenario = SCENARIO2,
-        models = (; integrated)
+        scenario = TEMPSCEN,
+        models = (; combined_pkpd)
     )[1, :]
 );
 
@@ -251,3 +250,299 @@ evaluate_efficacy(testpt)
 # set pd responses
 testpt["current_pd"] = (sdma = 60.0, plt = 300.0)
 evaluate_efficacy(testpt)
+
+##################################################################
+# evaluate_patient
+##################################################################
+
+# Testing NO_DOSE_ADJ = true; expect continue_therapy
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 20.0, plt = 300.0);
+testpt["scenario"] = (; SCENARIO..., NO_DOSE_ADJ = true);
+testpt["statdc"] = false;
+
+evaluate_patient(testpt) #* pass
+
+
+# Testing cycle_day = 7, but SKIP_CDAY7 = false; expect continue_therapy
+# cur_dose = 4, new_dose = 4
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 20.0, plt = 300.0);
+testpt["statdc"] = false; # normally created in loop, needed for continue_therapy
+testpt["cycle_day"] = 7
+
+evaluate_patient(testpt) #* pass
+
+
+# Testing cycle_day = 7, but SKIP_CDAY7 = false; expect continue_therapy, but from last condition
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 20.0, plt = 300.0);
+testpt["statdc"] = false; # normally created in loop, needed for continue_therapy
+testpt["cycle_day"] = 7
+
+evaluate_patient(testpt) #* pass
+
+
+# Testing cycle_day = 7, SKIP_CDAY7 = true; expect continue_therapy 1
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 20.0, plt = 300.0);
+testpt["statdc"] = false; # normally created in loop, needed for continue_therapy
+testpt["cycle_day"] = 7
+testpt["scenario"] = (; SCENARIO..., SKIP_CDAY7 = true)
+
+evaluate_patient(testpt) #* pass
+
+# Testing cycle_day = 21; expect continue_therapy 1
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 20.0, plt = 300.0);
+testpt["statdc"] = false; # normally created in loop, needed for continue_therapy
+testpt["cycle_day"] = 21
+
+evaluate_patient(testpt) #* pass
+
+# Testing G3 PLT; expect interrupt_therapy
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 20.0, plt = 45);
+testpt["cycle_day"] = 7
+
+evaluate_patient(testpt) #* pass
+
+# Testing G4 PLT; expect discontinue_therapy
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 20.0, plt = 20);
+testpt["cycle_day"] = 7
+
+evaluate_patient(testpt) #* pass
+
+# Testing restart after interrupt; expect restart_therapy at 1 level lower
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 20.0, plt = 55);
+testpt["cycle_day"] = 7
+testpt["lnzdose"] = [4.0]
+testpt["interrupt"] = true
+
+evaluate_patient(testpt) #* pass
+
+# Testing G2 taper, dose_tapered_in_cycle = false; expect modify_therapy with taper by 1 level
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 20.0, plt = 55);
+testpt["cycle_day"] = 7
+testpt["scenario"] = (; SCENARIO..., ADJUST_G2 = true)
+
+
+evaluate_patient(testpt) #* pass
+
+# Testing G2 taper, dose_tapered_in_cycle = false; expect continue_therapy 2 b/c already tapered during cycle
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 20.0, plt = 55);
+testpt["cycle_day"] = 7
+testpt["scenario"] = (; SCENARIO..., ADJUST_G2 = true)
+testpt["dose_tapered_in_cycle"] = true
+testpt["statdc"] = false
+
+evaluate_patient(testpt) #* pass
+
+# Testing CDAY28 evaluation; expect evaluate_therapy (no need to eval output here since tested thoroughly above)
+testpt = reset_patient()
+testpt["current_pd"] = (sdma = 40.0, plt = 55);
+testpt["sdma0"] = 100.0
+testpt["cycle_day"] = 28
+
+evaluate_patient(testpt) #* pass
+
+##################################################################
+# sim_trial
+##################################################################
+
+# helper function to order cols and compare dfs
+function compare_df(df1, df2)
+    select!(df1, :cycle, :day, :cycle_day, All())
+    select!(df2, :cycle, :day, :cycle_day, All())
+    if df1 == df2
+        return true
+    else  
+        isapprox.(df1,df2)
+    end
+end
+
+# standardized values to check control flow
+SAFE = 110; #Normal PLT
+G2 = 65; # G2 safety event
+G3 = 45; # G3 safety event
+G4 = 15; # G4 safety event
+PASS = 20; # from baseline an 80% reduction in SDMA (≥78% expected; PASS)
+FAIL = 40; # from baseline a 60% reduction in SDMA (≥78% expected; FAIL)
+
+#=
+ * 1 Cycle(s)
+ * No safety events
+ * Day 7 evaluation
+ * No G2 adjustment
+ * ≥78% reduction (PASS), no adjustment at CDAY28
+=#
+tp = [
+    (sdma=0,plt=0,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=0,cycle_day=0,current_dose_dur=0),
+    (sdma=FAIL,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=7,cycle_day=7,current_dose_dur=7),
+    (sdma=FAIL,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=14,cycle_day=14,current_dose_dur=14),
+    (sdma=FAIL,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=21,cycle_day=21,current_dose_dur=21),
+    (sdma=PASS,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=28,cycle_day=28,current_dose_dur=28),
+]
+
+testpt = reset_patient()
+testpt["sdma0"] = 100.0;
+testpt["plt0"] = 300.0;
+testpt["profile"] = tp
+
+# will modify test patient in place
+sim_trial_validation(testpt, 1)
+
+# should evaluate true when comparing the test profile with trial_events
+compare_df(DataFrame(tp), DataFrame(testpt["trial_events"]))
+
+
+#=
+ * 2 Cycle(s)
+ * No safety events
+ * CDay 7 evaluation
+ * No G2 adjustment
+ * ≥78% reduction (PASS), no adjustment at C1 CDAY28
+ * ≥78% reduction (PASS), no adjustment at C2 CDAY28
+=# 
+tp = [
+    (sdma=0,plt=0,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=0,cycle_day=0,current_dose_dur=0),
+    (sdma=FAIL,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=7,cycle_day=7,current_dose_dur=7),
+    (sdma=FAIL,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=14,cycle_day=14,current_dose_dur=14),
+    (sdma=FAIL,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=21,cycle_day=21,current_dose_dur=21),
+    (sdma=PASS,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=28,cycle_day=28,current_dose_dur=28),
+    (sdma=PASS,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=35,cycle_day=7,current_dose_dur=35),
+    (sdma=PASS,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=42,cycle_day=14,current_dose_dur=42),
+    (sdma=PASS,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=49,cycle_day=21,current_dose_dur=49),
+    (sdma=PASS,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=56,cycle_day=28,current_dose_dur=56),
+]
+
+testpt = reset_patient()
+testpt["sdma0"] = 100.0;
+testpt["plt0"] = 300.0;
+testpt["profile"] = tp
+
+# will modify test patient in place
+sim_trial_validation(testpt, 2)
+
+# should evaluate true when comparing the test profile with trial_events
+compare_df(DataFrame(tp), DataFrame(testpt["trial_events"]))
+
+
+#=
+ * 2 Cycle(s)
+ * G3 safety at TD14, rebound at TD21
+ * CDay 7 evaluation
+ * No G2 adjustment
+ * <78% reduction (FAIL), but NO adjustment at C1 CDAY28 b/c of previous taper
+ * <78% reduction (FAIL), increase by 1 dose level C2 CDAY28
+=# 
+tp = [
+    (sdma=0,plt=0,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=0,cycle_day=0,current_dose_dur=0),
+    (sdma=FAIL,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=7,cycle_day=7,current_dose_dur=7),
+    #* safety event
+    (sdma=FAIL,plt=G3,cur_dose=4.0,new_dose=0.0,recheck=7,statdc=false,interrupt=true,cycle=1,day=14,cycle_day=14,current_dose_dur=14),
+    (sdma=FAIL,plt=SAFE,cur_dose=0.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=21,cycle_day=14,current_dose_dur=7),
+    (sdma=FAIL,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=28,cycle_day=21,current_dose_dur=7),
+    #* fail, but no titration b/c tapered during cycle
+    (sdma=FAIL,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=35,cycle_day=28,current_dose_dur=14),
+    (sdma=PASS,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=42,cycle_day=7,current_dose_dur=21),
+    (sdma=PASS,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=49,cycle_day=14,current_dose_dur=28),
+    (sdma=PASS,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=56,cycle_day=21,current_dose_dur=35),
+    #* fail with titration because in subsequent cycle
+    (sdma=FAIL,plt=SAFE,cur_dose=2.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=63,cycle_day=28,current_dose_dur=42),
+]
+
+testpt = reset_patient()
+testpt["sdma0"] = 100.0;
+testpt["plt0"] = 300.0;
+testpt["profile"] = tp
+
+# will modify test patient in place
+sim_trial_validation(testpt, 2)
+
+# should evaluate true when comparing the test profile with trial_events
+compare_df(DataFrame(tp), DataFrame(testpt["trial_events"]))
+
+
+#=
+ * 2 Cycle(s)
+ * No safety events
+ * CDay 7 evaluation
+ * G2 adjustment 
+ * <78% reduction (FAIL), but NO adjustment at C1 CDAY28 b/c of G2 adjustment
+ * <78% reduction (FAIL), increase by 1 dose level C2 CDAY28
+=# 
+tp = [
+    (sdma=0,plt=0,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=0,cycle_day=0,current_dose_dur=0),
+    #* Immediate taper b/c of G2 rule
+    (sdma=FAIL,plt=G2,cur_dose=4.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=7,cycle_day=7,current_dose_dur=7),
+    (sdma=FAIL,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=14,cycle_day=14,current_dose_dur=7),
+    (sdma=FAIL,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=21,cycle_day=21,current_dose_dur=14),
+    #* Failing on CDAY 28, but no titration b/c of G2 adjustment
+    (sdma=FAIL,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=28,cycle_day=28,current_dose_dur=21),
+    (sdma=PASS,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=35,cycle_day=7,current_dose_dur=28),
+    (sdma=PASS,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=42,cycle_day=14,current_dose_dur=35),
+    (sdma=PASS,plt=SAFE,cur_dose=2.0,new_dose=2.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=49,cycle_day=21,current_dose_dur=42),
+    #* Failing, titrate 1 dose level
+    (sdma=FAIL,plt=SAFE,cur_dose=2.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=56,cycle_day=28,current_dose_dur=49),
+]
+
+testpt = reset_patient()
+testpt["sdma0"] = 100.0;
+testpt["plt0"] = 300.0;
+testpt["scenario"] = (; SCENARIO..., G2_ADJUST = true)
+testpt["profile"] = tp
+
+# will modify test patient in place
+sim_trial_validation(testpt, 2)
+
+# should evaluate true when comparing the test profile with trial_events
+compare_df(DataFrame(tp), DataFrame(testpt["trial_events"]))
+
+
+#=
+ * 3 Cycle(s)
+ * No safety events
+ * No CDay 7 evaluation
+ * G2 adjustment 
+ * <78% reduction (FAIL), titatre 1 level
+ * ≥78% reduction (PASS), unchanged
+ * 60% reduction would normally fail, but passess with offset (see below), unchanged
+=# 
+tp = [
+    (sdma=0,plt=0,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=0,cycle_day=0,current_dose_dur=0),
+    #* "Safety" event but SKIP_CDAY7 active so dose will be unchanged
+    (sdma=FAIL,plt=G3,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=7,cycle_day=7,current_dose_dur=7),
+    (sdma=FAIL,plt=SAFE,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=14,cycle_day=14,current_dose_dur=14),
+    #* "Safety" event but no evaluation on CDAY21 so dose will be unchanged
+    (sdma=FAIL,plt=G3,cur_dose=4.0,new_dose=4.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=21,cycle_day=21,current_dose_dur=21),
+    #* Failing, titrate 1 dose level
+    (sdma=FAIL,plt=SAFE,cur_dose=4.0,new_dose=6.0,recheck=7,statdc=false,interrupt=false,cycle=1,day=28,cycle_day=28,current_dose_dur=28),
+    (sdma=PASS,plt=SAFE,cur_dose=6.0,new_dose=6.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=35,cycle_day=7,current_dose_dur=7),
+    (sdma=PASS,plt=SAFE,cur_dose=6.0,new_dose=6.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=42,cycle_day=14,current_dose_dur=14),
+    (sdma=PASS,plt=SAFE,cur_dose=6.0,new_dose=6.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=49,cycle_day=21,current_dose_dur=21),
+    #* This should activate at_goal_prev_cycle
+    (sdma=PASS,plt=SAFE,cur_dose=6.0,new_dose=6.0,recheck=7,statdc=false,interrupt=false,cycle=2,day=56,cycle_day=28,current_dose_dur=28),
+    (sdma=PASS,plt=SAFE,cur_dose=6.0,new_dose=6.0,recheck=7,statdc=false,interrupt=false,cycle=3,day=63,cycle_day=7,current_dose_dur=35),
+    (sdma=PASS,plt=SAFE,cur_dose=6.0,new_dose=6.0,recheck=7,statdc=false,interrupt=false,cycle=3,day=70,cycle_day=14,current_dose_dur=42),
+    (sdma=PASS,plt=SAFE,cur_dose=6.0,new_dose=6.0,recheck=7,statdc=false,interrupt=false,cycle=3,day=77,cycle_day=21,current_dose_dur=49),
+    #* SDMA = 40 (ie 60% red from baseline) which should trigger titration, but won't because of target offset (lowers %red needed to meet efficacy)
+    #* to ensure that increases in SDMA from RUV after initial success don't lead to titration and put the patient at risk for SAE
+    (sdma=FAIL,plt=SAFE,cur_dose=6.0,new_dose=6.0,recheck=7,statdc=false,interrupt=false,cycle=3,day=84,cycle_day=28,current_dose_dur=56),
+]
+
+testpt = reset_patient()
+testpt["sdma0"] = 100.0;
+testpt["plt0"] = 300.0;
+testpt["scenario"] = (; SCENARIO..., G2_ADJUST = true, SKIP_CDAY7 = true, EFF_TARGET_OFFSET = 30)
+testpt["profile"] = tp
+
+# will modify test patient in place
+sim_trial_validation(testpt, 3)
+
+# should evaluate true when comparing the test profile with trial_events
+compare_df(DataFrame(tp), DataFrame(testpt["trial_events"]))
